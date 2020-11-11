61b36e94d0a0571ca3e59692479917c96f3bec9c
sari shteren <sh6197174@gmail.com>
2020-11-11 13:41:02 +0200

    login

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,3 +1,12 @@
-export class user {
-    constructor() { }
+export class User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    codeMosad: number;
+    phone?: string;
+    email?: string;
+
+    // constructor(id: string, firstName: string, lastName: string, codeMosad: number, phone?: string, email?: string,) {
+
+    // }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { promise } from 'protractor';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  u: User = { codeMosad: 1, firstName: "dasd", id: "23131", lastName: "asdad" };
+
+  users: User[] =
+    [{ codeMosad: 1, firstName: "sari", id: "111", lastName: "gutman" },
+    { codeMosad: 1, firstName: "sari", id: "222", lastName: "shteren" },
+    { codeMosad: 3, firstName: "adi", id: "333", lastName: "revivo" },
+    { codeMosad: 1, firstName: "naomi", id: "4444", lastName: "asdad" }
+    ];
   constructor() { }
+  login(u: User): Promise<User> {
+    return Promise.resolve(this.users.find(user => user.id == u.id));
+
+  }
 }
